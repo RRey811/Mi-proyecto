e06f8b14ae471e624d47b887656859a98034eba3
mi proyecto <mayorgavera8@gmail.com>
2024-11-17 17:46:57 -0500

    modifique mi proyecto

---
+++ script.js
@@ -0,0 +1,8 @@
+// Script simple para la validación del formulario
+
+document.getElementById('form-contacto').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    // Simula el envío de un mensaje
+    alert('Gracias por tu mensaje. Te responderé pronto.');
+});
